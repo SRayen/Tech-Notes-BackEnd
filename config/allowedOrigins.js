620fdc5d6872f1fab7c085566146bb16aa2e8058
Selmen Rayen <selmanerayene95@gmail.com>
2023-05-26 01:16:17 +0100

    fix allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://technotes.onrender.com" , "https://technotes-hl4s.onrender.com" , "https://technotes-api-73dz.onrender.com"];
+const allowedOrigins = [
+  "https://technotes.onrender.com",
+  "https://technotes-hl4s.onrender.com",
+
+];
 
 module.exports = allowedOrigins;
